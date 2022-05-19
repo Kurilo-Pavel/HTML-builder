d9fb44daef41bf6faa5338323c6535a00a51b0ab
Pavel <kurilo.pavel@mail.ru>
2022-05-19 11:57:43 +0300

    feat: complet task 1(THU, May 19, 2022, 11:57:30)

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+const path = require('node:path');
+let data = '';
+let readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+readableStream.on('data', chunk => data += chunk);
+readableStream.on('end', () => {
+  console.log(data);
+});
