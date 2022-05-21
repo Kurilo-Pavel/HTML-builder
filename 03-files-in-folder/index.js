20a316aa7c41f0c2d3f6f19a94a7ee9ee1630481
Pavel <kurilo.pavel@mail.ru>
2022-05-21 22:18:43 +0300

    feat: Complet task 4 (Sat, May 21, 2022, 22:18:30)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,8 +5,8 @@ fs.readdir(filePath, {encoding: 'utf-8', withFileTypes: true}, (err, files) => {
   if (err) throw err;
   files.forEach(file => {
     if (file.isFile()) {
-      const pathLink = path.join(__dirname, 'secret-folder', file.name);
-      fs.stat(pathLink, (err, stats) => {
+      const pathStat = path.join(__dirname, 'secret-folder', file.name);
+      fs.stat(pathStat, (err, stats) => {
         if (err) throw err;
         console.log(file.name.slice(0, file.name.lastIndexOf(path.extname(file.name))) +
           ' - ' + path.extname(file.name).slice(1) + ' - ' + stats.size/1024+' Кб');
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('node:path');
 
+const pathParentFile = path.join(__dirname);
+const pathNewFile = path.join(__dirname, 'files-copy');
+const pathFile = path.join(__dirname, 'files');
+
+fs.readdir(pathParentFile, 'utf-8', (err, files) => {
+  if (err) throw err;
+  if (files.some(file => file === 'files-copy')) {
+    fs.rm(pathNewFile,{ recursive: true, force: true },(err)=>{
+      if(err) throw err;
+      addDirection();
+    });
+  } else {addDirection();}
+});
+
+function addDirection() {
+  fs.mkdir(pathNewFile, {recursive: true}, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(pathFile, 'utf-8', (err, files) => {
+    if (err) throw err;
+    files.forEach(file => {
+      const pathOldFile = path.join(__dirname, 'files', file);
+      const pathNewFile = path.join(__dirname, 'files-copy', file);
+      fs.copyFile(pathOldFile, pathNewFile, (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+}
